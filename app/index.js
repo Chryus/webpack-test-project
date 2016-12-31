f4acec276eaf2e33a0fe24abf720cef8d791f8fa
Chris <chris.haack@me.com>
2016-12-31 09:36:03 -0500

    build avatar component via pure functions

--- app/index.js
+++ app/index.js
@@ -11,11 +11,60 @@ class Greeting extends React.Component {
   }
 }
 
-const user = {
+const USER = {
   name: "Maggie"
 };
 
 ReactDOM.render(
-  <Greeting name={user.name} />,
+  <Greeting name={USER.name} />,
   document.getElementById("app")
+);
+
+const USER_DATA = {
+  name: 'Chris Haaaaaaaack',
+  username: 'chryus',
+  image: 'https://avatars0.githubusercontent.com/u/5354390?v=3&s=460'
+}
+
+class ProfilePic extends React.Component {
+  render () {
+    return (
+      <img src={this.props.imageUrl} style={{width:100, height:100}}/>
+    );
+  }
+}
+
+class ProfileName extends React.Component {
+  render () {
+    return (
+      <div>{this.props.name}</div>
+    );
+  }
+}
+
+class ProfileLink extends React.Component {
+  render () {
+    return (
+      <a href={'https://www.github.com/' + this.props.username}>
+        {this.props.username}
+      </a>
+    );
+  }
+}
+
+class Avatar extends React.Component {
+  render () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Avatar user={USER_DATA} />,
+  document.getElementById("avatar-section")
 );
